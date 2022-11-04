1748c02207bb767162285a1a474f3df97b84134b
Massimo Vassalli <massimo.vassalli@gmail.com>
2022-11-04 00:17:43 +0000

    new algos

---
+++ protocols/cpoint/extthresh.py
@@ -0,0 +1,56 @@
+from ..panels import boxPanel
+import numpy as np
+from magicgui.widgets import  FloatSpinBox,SpinBox
+
+
+NAME = 'AutoThreshold'
+DESCRIPTION = 'Identify the CP by thresholding it over different degrees'
+DOI = ''
+
+import numpy as np
+from scipy.signal import savgol_filter,argrelmin
+
+class CP(boxPanel):  # Threshold
+    def create(self):
+        self.addParameter('ZeroRange',FloatSpinBox(value=500.0, name='ZeroRange', label='Range to set the zero [nN]',min=20))
+    def calculate(self, x,y):
+        
+        deg = 0 #self.getValue('Degree')
+        worky = np.copy(y)
+        #window = self.getValue('Window')
+        #if window%2 == 0:
+        #    window+=1            
+        #if deg > 0:
+        #    for k in range(deg):
+        #        worky = savgol_filter(worky,window,3,deriv=1)
+        
+        xtarget = np.min(x) + self.getValue('ZeroRange')*1e-9
+        jtarget = np.argmin( np.abs(x-xtarget) )
+        
+        #which direction?
+        if x[0]<x[-1]:
+            xlin = x[:jtarget]
+            ylin = worky[:jtarget]
+            m,q = np.polyfit(xlin,ylin,1)
+        else:
+            xlin = x[jtarget:]
+            ylin = worky[jtarget:]
+            m,q = np.polyfit(xlin,ylin,1)
+        
+        worky = worky-m*x-q
+        
+        differences = (worky[1:]+worky[:-1])/2
+        midpoints = np.array(list(set(differences)))
+        midpoints.sort()
+        
+        crossings = []
+        for threshold in midpoints[midpoints>0]:
+            crossings.append( np.sum( np.bitwise_and( (worky[1:]>threshold),(worky[:-1]<threshold) )))
+        crossings=np.array(crossings)
+        
+        inflection = midpoints[midpoints>0][np.where(crossings==1)[0][0]]
+        jcpguess = np.argmin( np.abs(differences-inflection) )+1
+        
+        xcp = x[jcpguess]
+        ycp = y[jcpguess]
+        return [xcp, ycp]
---
+++ protocols/emodels/cmax.py
@@ -0,0 +1,29 @@
+#import the main panels structure, required
+from ..panels import fitPanel
+#import here your procedure-specific modules, no requirements (numpy as an example)
+import numpy as np
+
+#Set here the details of the procedure
+NAME = 'LineMax' #Name, please keep it short as it will appear in the combo box of the user interface
+DESCRIPTION = 'Evaluate the average over a range and the maximum' #Free text
+DOI = '' #set a DOI of a publication you want/suggest to be cited, empty if no reference
+PARAMETERS = {'E [Pa]':"Average modulus",'Emax [Pa]':"Max modulus"}
+
+# Create your filter class by extending the main one
+# Additional methods can be created, if required
+class EModel(fitPanel):
+    def create(self):
+        # This function is required and describes the form to be created in the user interface 
+        # The last value is the initial value of the field; currently 3 types are supported: int, float and combo
+        # Parameters can be used as seeds or proper parameters (e.g. indentation depth ?) 
+        #self.addParameter('maxind','float','Max indentation [nm]',800)
+        #self.addParameter('poisson','float','Poisson ratio',0.5)
+        pass
+
+    def theory(self,x,*parameters):
+        return parameters[0]*np.ones(len(x))
+
+    def calculate(self, x,y):
+        full = self.curve._E
+        # This function gets the current x and y and returns the parameters.
+        return [np.average(y),np.max(full)]
