167227bb3600a13f144006f546cb27b251e57e61
Massimo Vassalli <massimo.vassalli@gmail.com>
2024-06-01 09:27:15 +0100

    Update andrei.py

--- tools/andrei.py
+++ tools/andrei.py
@@ -1,51 +1,56 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
 
 #open the file
 filename = '../testdata/test.hdf5'
 experiment = h5py.File(filename,'r')
 
-#list to store the curves
-curves = []
-
+fig,[ax1,ax2,ax3,ax4] = plt.subplots(1,4)
+ax1.set_xlabel('Time [s]')
+ax1.set_ylabel('Z [nm]')
+ax1.set_title('Piezo')
+ax2.set_xlabel('Time [s]')
+ax2.set_ylabel('Force [nN]')
+ax2.set_title('Force')
+ax2.set_xlabel('Position [nm]')
+ax2.set_ylabel('Force [nN]')
+ax2.set_title('F vs Z approach')
+ax4.set_xlabel('Indentation [nm]')
+ax4.set_ylabel('Force [nN]')
+ax4.set_title('F vs Ind approach')
+alpha=0.2
 #browse the experiment
 for struct_id in list(experiment.keys()):
     structure=experiment[struct_id]
-    
     #extrat the relevant info for the selected segment
     k = structure.attrs['spring_constant']
     selectedSegment = structure.attrs['selectedSegment']
-    F = np.array(structure[f'segment{selectedSegment}']['Force'])
-    Z = np.array(structure[f'segment{selectedSegment}']['Z'])
-    Time = np.array(structure[f'segment{selectedSegment}']['Time'])
+    nseg = structure.attrs['segments']
     
-    if 'cp' in structure.keys():
-        cp = np.array(structure['cp'])
-        
-        #normalise to the cp
-        F = F - cp[1]
-        Z = Z - cp[0]
-        iContact = np.argmin( Z** 2)
-        #extract the portion in contact and calculate the indentation
-        Force = F[iContact:]
-        Indentation = Z[iContact:] - Force / k
-        Delay = Time[iContact:]-Time[iContact]
-        #populate the array
-        curves.append([Delay,Indentation,Force])
 
+    for iseg in range(nseg):    
+        Force = np.array(structure[f'segment{iseg}']['Force'])
+        Zpos = np.array(structure[f'segment{iseg}']['Z'])
+        Time = np.array(structure[f'segment{iseg}']['Time'])
+        col = 'r'
+        if iseg==selectedSegment:
+            col='g'
+        ax1.plot(Time,Zpos*1e9,col,alpha=alpha)        
+        ax2.plot(Time,Force*1e9,col,alpha=alpha)
+        
+        if 'cp' in structure.keys():
+            cp = np.array(structure['cp'])
+            #normalise to the cp
+            Force = Force - cp[1]
+            Zpos = Zpos - cp[0]
+            Indentation = Zpos - Force/k
+            
+            if iseg==selectedSegment:
+                ax3.plot(Zpos,Force*1e9,'g',alpha=alpha)
+                icp = np.argmin(Zpos**2)
+                ax4.plot(Indentation[icp:],Force[icp:]*1e9,'g',alpha=alpha)        
+            
 experiment.close()
 
-#do something with the curves
-import matplotlib.pyplot as plt
-fig,[ax1,ax2,ax3] = plt.subplots(1,3)
-for c in curves:
-    ax1.plot(c[0],c[1],'r',alpha=0.4)
-    ax1.set_xlabel('Time [s]')
-    ax1.set_ylabel('Indentation [m]')
-    ax2.plot(c[0],c[2],'r',alpha=0.4)
-    ax2.set_xlabel('Time [s]')
-    ax2.set_ylabel('Force [N]')
-    ax3.plot(c[1],c[2],'r',alpha=0.4)
-    ax3.set_xlabel('Indentation [m]')
-    ax3.set_ylabel('Force [N]')
 plt.show()
